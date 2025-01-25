7857bd237d347377c398cc4f4ba18d2145420c63
Rafael Kachlon <rafaelkachlon@gmail.com>
2025-01-25 19:18:47 +0200

    replace a with Link

--- src/pages/Home/components/MovieList/MovieList.tsx
+++ src/pages/Home/components/MovieList/MovieList.tsx
@@ -1,12 +1,14 @@
 import Movie from '../../../../models/movie.ts';
 import './MovieList.scss';
 import StarIcon from '@mui/icons-material/Star';
+import { Link } from 'react-router';
 
 export const MovieList = (props: { movies: Movie[] }) => {
   return (
     <div className="movie-list">
+
       {props.movies.map((movie: Movie) => (
-        <a href={`/movies/${movie.id}`} key={movie.id} className="movie-item">
+        <Link to={`/movies/${movie.id}`} key={movie.id} className="movie-item">
           <img src={movie.poster} alt={movie.title}/>
           <div className="movie-title">{movie.title}</div>
           <div className="movie-duration">{movie.duration}</div>
@@ -14,7 +16,7 @@ export const MovieList = (props: { movies: Movie[] }) => {
             <StarIcon fontSize="small"/>
             <span>{movie.rating}</span>
           </div>
-        </a>
+        </Link>
       ))}
     </div>
   );
